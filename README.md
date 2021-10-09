121916adc470c88256939d6319947695f80d7416
Jordan Sussman <jsussman@workwithopal.com>
2021-10-09 14:58:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## README
+A responsive website showcasing my professional background to share with the world.
+
+![](website.png)
+
+Built with:
+```
+- HTML
+- CSS
+- JavaScript
+```
---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/* SCROLL HOME SOCIAL */
+sr.reveal('.home__social-icon',{ interval: 100}); 
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
