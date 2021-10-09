370d74cc9abb2bad1ae8bf78d2e76408b73af9bf
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2021-10-09 16:26:22 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /* SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
-    delay: 200,
+    delay: 400,
     origin: 'top',
     distance: '80px',
     duration: 2000,
@@ -8,8 +8,8 @@ const sr = ScrollReveal({
 });
 
 /* SCROLL HOME SOCIAL */
-sr.reveal('.home__social-icon',{ interval: 100}); 
+sr.reveal('.home__social-icon',{interval: 100}); 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
+sr.reveal('.about__img',{delay: 400}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400});
