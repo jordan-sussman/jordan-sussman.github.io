a291b64ca1404b27d9846b9badcb901e73b40da4
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2023-04-26 15:23:33 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ sr.reveal(".about__img",{delay: 600, origin: "left"});
 sr.reveal(".about__main",{delay: 400, origin: "right"});
 sr.reveal(".about__subtitle",{delay: 400});
 sr.reveal(".about__text",{delay: 400, origin: "right"});
-sr.reveal(".home__social-icon",{interval: 100, delay: 400, distance: "40px", origin: "bottom"});
+sr.reveal(".home__social-icon",{interval: 100, delay: 400, distance: "30px", origin: "bottom"});
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
