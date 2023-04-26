34d2e0453b60e3936cda32c52be691329381cf8a
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2023-04-26 15:21:27 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,11 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(".header__section",{delay: 200});
-sr.reveal(".home__social-icon",{interval: 100, delay: 400, origin: "bottom"}); 
-sr.reveal(".about__img",{delay: 600, origin: "left"}); 
-sr.reveal(".about__subtitle",{delay: 400}); 
+sr.reveal(".about__img",{delay: 600, origin: "left"});
 sr.reveal(".about__main",{delay: 400, origin: "right"});
+sr.reveal(".about__subtitle",{delay: 400});
 sr.reveal(".about__text",{delay: 400, origin: "right"});
+sr.reveal(".home__social-icon",{interval: 100, delay: 400, distance: "40px", origin: "bottom"});
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
