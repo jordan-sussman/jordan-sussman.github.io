7b14e343dac859976ac4a14b295c2040f824e7a1
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2024-04-16 12:18:09 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 /* SCROLL REVEAL ANIMATIONS */
 const sr = ScrollReveal({
-  distance: "100px",
+  distance: "80px",
   duration: 800,
   reset: false,
 });
 
 sr.reveal(".about__main", { delay: 400, origin: "bottom" });
+sr.reveal(".header", { delay: 500, origin: "top" });
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
