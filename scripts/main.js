65b2805f6b93bae4c685dd42c1f8d3e345653f52
Jordan Sussman <52428171+jordan-sussman@users.noreply.github.com>
2025-03-15 11:19:25 -0700

    update reveal script

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 /* SCROLL REVEAL ANIMATIONS */
 const sr = ScrollReveal({
-  distance: "80px",
+  distance: "30px",
   duration: 800,
   reset: false,
 });
 
-sr.reveal(".about__main", { delay: 400, origin: "bottom" });
-sr.reveal(".header", { delay: 500, origin: "top" });
+sr.reveal(".about__main", { delay: 500, origin: "bottom" });
+sr.reveal(".header__container", { delay: 400, origin: "bottom" });
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
